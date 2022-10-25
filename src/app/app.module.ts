0af539f5914491ff03612068a431dacdb0812fba
Shameer Sheriff <shameersheriff100@gmail.com>
2022-10-26 01:24:39 +0530

    Create main section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,29 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopNavComponent } from './components/shared/top-nav/top-nav.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { MainSectionComponent } from './components/shared/main-section/main-section.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavComponent,
-    FooterComponent
+    FooterComponent,
+    MainSectionComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
