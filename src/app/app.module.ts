e8f0076176c0f15899c227574bdcb0350f810d80
Shameer Sheriff <shameersheriff100@gmail.com>
2022-10-25 17:45:32 +0530

    Create top nav and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopNavComponent } from './components/shared/top-nav/top-nav.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
