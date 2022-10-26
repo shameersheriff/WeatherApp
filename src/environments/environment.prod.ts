a1b3f9fdf4dd630e3c746c5935539ddea3bc19c3
Shameer Sheriff <shameersheriff100@gmail.com>
2022-10-26 23:01:13 +0530

    Update env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  openWeatherMapAPI_URL: 'https://api.openweathermap.org/data/2.5/weather?q=',
+  openWeatherMapAPI_AppID: 'e7ca04393151d01144864695526dd6a0'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  openWeatherMapAPI_URL: 'https://api.openweathermap.org/data/2.5/weather?q=',
+  openWeatherMapAPI_AppID: 'e7ca04393151d01144864695526dd6a0'
 };
 
 /*
