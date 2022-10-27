19834f82206b673f59636a62d54d7536c72f4fcc
Shameer Sheriff <shameersheriff100@gmail.com>
2022-10-27 08:36:26 +0530

    Apply style youch ups, Refactoring unnecessary code

--- src/app/components/shared/main-section/main-section.component.ts
+++ src/app/components/shared/main-section/main-section.component.ts
@@ -46,24 +46,4 @@ export class MainSectionComponent implements OnInit {
       this.structuredForecastList = this.weatherMapService.getStructuredForecastByDay(this.forecast);
     });
   }
-
-  defaultLocation(): void {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.showPosition);
-    } else {
-      console.log('Geolocation is not supported by this browser.');
-    }
-  }
-
-  showPosition(position: any): void {
-    console.log(
-      'Latitude: ' +
-        position.coords.latitude +
-        ' Longitude: ' +
-        position.coords.longitude
-    );
-    this.weatherMapService
-      .searchByCoordinates(position.coords.latitude, position.coords.longitude)
-      .subscribe((data: any) => {});
-  }
 }
